b20e04bcb15470a513497cba6cc7909c56b5ec79
yunusoktay <ynsokty.3@gmail.com>
2022-02-24 19:35:37 +0300

    Initial Commit

---
+++ MapApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapApplication
+//
+//  Created by yunus oktay on 24.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
